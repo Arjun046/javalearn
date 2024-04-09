c11fa8c250ee90ac09f863a2fa610c2fa537a93c
Arjun Kumar Suthar <arjunsuthar2001@gmail.com>
2024-04-09 22:04:43 +0530

    kk

---
+++ SpringJdbC/SpringJdbC/src/main/resources/Schema.sql
@@ -0,0 +1,8 @@
+create TABLE employee(
+   id int primary KEY,
+   firstName varchar(50),
+   lastName varchar(30),
+   Designation varchar(20),
+   Address varchar(50),
+   ContactNo varchar(10)
+);
